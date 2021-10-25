2d20c4f7318b36a99fdae2d2b582cb93be0639e8
Dan-DH <daniel.diaz.hdez@gmail.com>
2021-10-25 19:05:59 +0200

    cookie clicker v0.1

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@
     //setting the multiplier factor. Default is one, increases when player buys an upgrade
     localStorage.hasOwnProperty("mult") ? true : localStorage.setItem("mult", 1);
 
-    //set number of autoclickers, check if there is a 'score' variable already in local storage, creates one if not
-    const team = {
+    //set number of autoclickers, check if there is a 'team' variable already in local storage, creates one if not
+    //localStorage can't hold objects, so we need to use JSON.stringify to save them, and parse them back when we need to use them
+    var team = {
         0: 0,
         1: 0,
         2: 0
     }
 
-    localStorage.hasOwnProperty("team") ? true : localStorage.setItem("team", JSON.stringify(team)); 
-    var parseTeam = localStorage.getItem("team");
-    console.log(JSON.parse(parseTeam))
+    localStorage.hasOwnProperty("team") ? team = JSON.parse(localStorage.getItem("team")) : localStorage.setItem("team", JSON.stringify(team));
+    //var parseTeam = JSON.parse(localStorage.getItem("team")); //not needed atm
 
     //display autoclickers
     document.getElementById("alisan").innerHTML = `Alisan(s) making cookies: ${team[0]}`;
@@ -38,14 +38,17 @@
         2 : 12
     }
 
-    //multiplier and autoclicker status (1 used / 0 not used)
-    const multiplierStatus = {
+    //set multiplier status --max 1 of each multiplier can be bought
+    var multiplierStatus = {
         "x2": 0,
         "x3": 0,
         "x4": 0
     }
 
-    localStorage.hasOwnProperty("multiplierStatus") ? true : localStorage.setItem("multiplierStatus", JSON.stringify(multiplierStatus)); 
+    //check if there is a 'multiplierStatus' variable already in local storage, create one if not
+    localStorage.hasOwnProperty("multiplierStatus") ? multiplierStatus = JSON.parse(localStorage.getItem("multiplierStatus")) : localStorage.setItem("multiplierStatus", JSON.stringify(multiplierStatus)); 
+   //var parseMultiplierStatus = JSON.parse(localStorage.getItem("multiplierStatus")); //not needed atm
+    
 
     //cookie click event
     document.getElementById("cookie").addEventListener("click",() => {
@@ -57,69 +60,56 @@
     //multiplier button click event
     document.querySelectorAll("button.multiplier").forEach(btn =>
         btn.addEventListener("click", () => {
-                if (multiplierStatus[btn.id] === 0 && localStorage.score >= multiplierPrices[btn.id]) {
-                    multiplierStatus[btn.id] = 1;
-                    mult += 1;
-                    localStorage.score -= multiplierPrices[btn.id];
-                    document.getElementById("score").innerHTML = localStorage.score;                   
-                    console.log(localStorage.score); //test log, to be removed
-                }
-            }),
+            if (multiplierStatus[btn.id] === 0 && localStorage.score >= multiplierPrices[btn.id]) {
+                multiplierStatus[btn.id] = 1;
+                localStorage.setItem("multiplierStatus", JSON.stringify(multiplierStatus));
+                localStorage.mult = parseInt(localStorage.mult) + 1; //if we chose this option, buttons must unlock in order
+                //localStorage.mult = parseInt(localStorage.mult) + parseInt(btn.id.charAt(1)) - 1;
+                localStorage.score -= multiplierPrices[btn.id];
+                document.getElementById("score").innerHTML = localStorage.score;                   
+                console.log(localStorage.score); //test log, to be removed
+            }
+        }),
     );
 
     //autoclicker button click event
     document.querySelectorAll("button.autoclicker").forEach(btn =>
-        btn.addEventListener(
-            "click",
-            () => {
-                if (localStorage.score >= autoClickerPrices[btn.id]) {
-                    localStorage.score -= autoClickerPrices[btn.id];
-                    document.getElementById("score").innerHTML = localStorage.score;
-                    team[btn.id] += 1; 
-                    autoclickerFunction(btn.id);
-                    console.log(localStorage.score); //test log, to be removed
-                }
+        btn.addEventListener("click", () => {
+            if (localStorage.score >= autoClickerPrices[btn.id]) {
+                localStorage.score -= autoClickerPrices[btn.id];
+                document.getElementById("score").innerHTML = localStorage.score;
+                team[btn.id] += 1; 
+                localStorage.setItem("team", JSON.stringify(team))
+                autoclickerFunction(btn.id);
+                console.log(localStorage.score); //test log, to be removed
             }
+        }
         ),
     );
-
-    //restart button click event
-     document.getElementById("startAgain").addEventListener("click",() => {
-        //set score back to 0
-        localStorage.setItem("score", 0); 
-        document.getElementById("score").innerHTML = localStorage.score;
-
-        //set statuses back to false
-        for (var item in multiplierStatus) {
-            multiplierStatus[item] = 0;
-        }
-
-        //set autoclickers back to 0 and display them
-        for (var item in team) {
-            team[item] = 0;
-        }
-
-        document.getElementById("alisan").innerHTML = `Alisan(s) baking cookies: ${team[0]}`;
-        document.getElementById("daniel").innerHTML = `Daniel(s) baking cookies: ${team[1]}`;
-        document.getElementById("shivani").innerHTML = `Shivani(s) baking cookies: ${team[2]}`;
-
-        //clear all intervals
-        interval.map(i => clearInterval(i));
-
-        //remove all localStorage items
-        localStorage.clear();
-    });   
 
     function autoclickerFunction(timer) {
         interval.push(setInterval(() => {
             localStorage.score = parseInt(localStorage.score) + (parseInt(timer) + 3);
             document.getElementById("score").innerHTML = localStorage.score;
-        }, 2000 * (timer +1)))
+            console.log(localStorage.score)
+        }, 2000 * (timer)))
 
         document.getElementById("alisan").innerHTML = `Alisan(s) baking cookies: ${team[0]}`;
         document.getElementById("daniel").innerHTML = `Daniel(s) baking cookies: ${team[1]}`;
         document.getElementById("shivani").innerHTML = `Shivani(s) baking cookies: ${team[2]}`;
-    }
+    };
+
+    //restart button click event
+     document.getElementById("startAgain").addEventListener("click",() => {
+        //clear all intervals
+        interval.map(i => clearInterval(i));
+
+        //remove all localStorage items
+        localStorage.clear();
+
+        //refresh page to trigger script from the beginning
+        window.location.reload();
+    });   
 
 /* 
     let buttonCookie = document.getElementsByClassName("click")[0];
