1c4088fec93bcbb051c617f987e0fb8ca662da19
Dan-DH <daniel.diaz.hdez@gmail.com>
2021-10-26 14:02:11 +0200

    cookie rain animation

--- js/script.js
+++ js/script.js
@@ -187,5 +187,30 @@
 
         //refresh page to trigger script from the beginning
         window.location.reload();
-    });   
+    }); 
+    
+    //cookie rain
+    const flake = document.querySelector(".flake");
+    const container = document.querySelector(".cookieRain");
+
+    function createFlake() {
+        // cloning the flake node
+        const clone = flake.cloneNode(true);
+
+        // creating left padding
+        clone.style.paddingLeft = Math.random() * 20 + "%";
+
+        // animation duration between 3-5
+        clone.style.animationDuration = Math.random() * 7 + 4 + "s";
+        clone.style.opacity = Math.random() * 1;
+        container.append(clone); // adding clone flake to container
+    }
+
+    // to create more flakes decrease 100
+    const s = setInterval(createFlake, 50);
+
+
+    setTimeout(() => {
+    clearInterval(s);
+    }, 3000); // flake creation stops after 3000 milliseconds or 3s
 })();
